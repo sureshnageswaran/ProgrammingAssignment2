42ea8cc07aa05b63c50bc2108ff6bb4fca74747e
Suresh Nageswaran <sureshnageswaran@yahoo.com>
2014-07-27 12:11:09 -0400

    Update cachematrix.r
    Both function bodies and comments are added. This is a working and tested code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,83 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Author : Suresh Nageswaran
+## Email: sureshnageswaran@yahoo.com
 
-## Write a short comment describing this function
+## Function makeCacheMatrix  takes in a matrix and 
+## and returns an object who's inverse can be cached.
 
-makeCacheMatrix <- function(x = matrix()) {
+## Invoke like so: 
+##
+## matrixT <- matrix(c(-1, 0, 1, 2), 2, 2)
+##
+## objTest <- makeCacheMatrix(matrixT)
+##
+## If the determinant of the input matrix is zero
+## or if it is a non-square matrix, the execution terminates
+## 
+## The returned object has 4 "public" functions that can
+## modify it, namely : set(), get(), setInverse() and getInverse()
 
+makeCacheMatrix <- function(x = matrix())
+{
+  if(!is.matrix(x)) 
+  {
+    stop("Input must be a matrix object only")
+  }
+  if (nrow(x) != ncol(x))
+  {
+    stop("Matrix inverse cannot be calculated for non-square matrices")
+  }
+  iDet <- det(x)	
+  if( iDet == 0)
+  {
+    stop("Matrix determinant is zero. This is a non-invertible matrix")
+  }
+  iInv <- NULL
+  
+  set <- function(matrix)
+  {
+    x <<- matrix
+    iDet <- det(x)
+    if( iDet == 0 || nrow(x) != ncol(x) || !is.matrix(x))
+    {
+      stop("Unusable matrix")
+    }
+    iInv <<- NULL	
+  }
+  get <- function()
+  {
+    return(x)
+  }
+  setInverse <- function(Inverse)
+  {
+    iInv <<- Inverse
+  }
+  getInverse <- function()
+  {
+    return(iInv)
+  }
+  
+  list( set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  
 }
 
+## cacheSolve is a function that operates on an object that is
+## of the type created by the makeCacheMatrix() call.
+## The first time cacheSolve is invoked, it calculates the matrix
+## inverse. 
+## The second time onwards whenever cacheSolve is invoked, the
+## cached matrix inverse stored in the object instance is 
+## returned.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(objX, ...)
+{
+  Inv <- objX$getInverse()
+  if(!is.null(Inv))
+  {
+    message("Printing cached matrix inverse: ")
+    return(Inv)
+  }
+  x <- objX$get()
+  Inv <- solve(x)
+  objX$setInverse(Inv)
+  print(Inv)
 }
